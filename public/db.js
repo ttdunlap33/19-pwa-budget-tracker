560af589fc97a834ee860cdfe08e803e3dce9b18
Thomas Dunlap <ttdunlap33@gmail.com>
2021-07-02 15:18:38 -0600

    manifests and some offline functionality

---
+++ public/db.js
@@ -0,0 +1,93 @@
+let db
+let budgetVersion
+
+// Create a new db request for a "budget" database.
+const request = indexedDB.open('myFirstDatabase', budgetVersion || 21)
+
+request.onupgradeneeded = function (e) {
+  console.log('Upgrade needed in IndexDB')
+
+  const { oldVersion } = e
+  const newVersion = e.newVersion || db.version
+
+  console.log(`DB Updated from version ${oldVersion} to ${newVersion}`)
+
+  db = e.target.result
+
+  if (db.objectStoreNames.length === 0) {
+    db.createObjectStore('newTransaction', { autoIncrement: true })
+  }
+}
+
+request.onerror = function (e) {
+  console.log(`Woops! ${e.target.errorCode}`)
+}
+
+function checkDatabase() {
+  console.log('check db invoked')
+
+  // Open a transaction on your newTransaction db
+  let transaction = db.transaction(['newTransaction'], 'readwrite')
+
+  // access your newTransaction object
+  const store = transaction.objectStore('newTransaction')
+
+  // Get all records from store and set to a variable
+  const getAll = store.getAll()
+
+  // If the request was successful
+  getAll.onsuccess = function () {
+    // If there are items in the store, we need to bulk add them when we are back online
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction/bulk', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => response.json())
+        .then((res) => {
+          // If our returned response is not empty
+          if (res.length !== 0) {
+            // Open another transaction to newTransaction with the ability to read and write
+            transaction = db.transaction(['newTransaction'], 'readwrite')
+
+            // Assign the current store to a variable
+            const currentStore = transaction.objectStore('newTransaction')
+
+            // Clear existing entries because our bulk add was successful
+            currentStore.clear()
+            console.log('Clearing store 🧹')
+          }
+        })
+    }
+  }
+}
+
+request.onsuccess = function (e) {
+  console.log('success')
+  db = e.target.result
+
+  // Check if app is online before reading from db
+  if (navigator.onLine) {
+    console.log('Backend online! 🗄️')
+    checkDatabase()
+  }
+}
+
+const saveRecord = (record) => {
+  console.log('Save record invoked')
+  // Create a transaction on the newTransaction db with readwrite access
+  const transaction = db.transaction(['newTransaction'], 'readwrite')
+
+  // Access your newTransaction object store
+  const store = transaction.objectStore('newTransaction')
+
+  // Add record to your store with add method.
+  store.add(record)
+}
+
+// Listen for app coming back online
+window.addEventListener('online', checkDatabase)
