d1e351c705e96c1456b7f4be70bbb68fb5fe187d
Thomas Dunlap <ttdunlap33@gmail.com>
2021-07-02 14:41:58 -0600

    atlas setup

--- server.js
+++ server.js
@@ -15,10 +15,26 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
+const uri = "mongodb+srv://root:root@cluster0.h5jen.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(uri, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
+
+
+// const { MongoClient } = require('mongodb');
+// const uri = "mongodb+srv://root:root@cluster0.h5jen.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
 
 // routes
 app.use(require("./routes/api.js"));
